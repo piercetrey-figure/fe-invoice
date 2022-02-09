909d9fef44118df7d94cd24acc61cce13455f7a3
hyperschwartz <jschwartz@figure.com>
2022-02-09 11:21:03 -0800

    Remove public key requirement for onboarding

--- src/hooks/useCreateInvoice.ts
+++ src/hooks/useCreateInvoice.ts
@@ -1,7 +1,6 @@
 import { BASE_URL } from "consts"
 import { Invoice } from "../proto/invoice_protos_pb"
 import { useWalletConnect } from '@provenanceio/walletconnect-js'
-import { urlSafeBase64ToBase64 } from "../util"
 
 export interface PayablesContractExecutionDetail {
     payableType: string,
@@ -24,14 +23,13 @@ export interface CreateInvoiceResponse {
 export const useCreateInvoice = () => {
     const { walletConnectState } = useWalletConnect()
 
-    const { address, publicKey } = walletConnectState
+    const { address } = walletConnectState
     return {
         onCreate: async (invoice: Invoice): Promise<CreateInvoiceResponse> => {
             const { lineItemsList: lineItems, ...rest } = invoice.toObject()
             const body = JSON.stringify({ ...rest, lineItems })
             const res = await fetch(`${BASE_URL}/invoices/onboard`, { method: 'POST', body, headers: {
                 'Content-Type': 'application/json',
-                'x-public-key': urlSafeBase64ToBase64(publicKey),
                 'x-address': address,
             } })
             const resBody = await res.json()
